1fc6b004e186a5d80c30988f1484bdbca4433589
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2024-02-07 08:45:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 
 # Read image
 cap = cv2.VideoCapture(0)
+#img = cv2.imread('Parallellines.png')
 slopes = {}
 while True:
     ret, frame = cap.read()
+
     # Convert image to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -23,7 +25,7 @@ while True:
         np.pi / 180,  # Angle resolution in radians
         threshold=100,  # Min number of votes for valid line
         minLineLength=5,  # Min allowed length of line
-        maxLineGap=10  # Max allowed gap between line for joining them
+        maxLineGap=90  # Max allowed gap between line for joining them
 
     )
 
@@ -35,14 +37,47 @@ while True:
             x1, y1, x2, y2 = points[0]
 
             slope = (y2 - y1)/(x2 - x1)
-            slopes[points] = slope
+            slopes[slope] = x1, y1, x2, y2
 
-            print(slopes)
+            print(slopes[slope])
 
-            # Draw the lines to connect points
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            # Maintain a simples lookup list for points
-            lines_list.append([(x1, y1), (x2, y2)])
+            for x in slopes:
+                if x == slope:
+                    matching = slopes[x]
+                    print('found match')
+                    # # Draw the lines to connect points
+                    cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) #original line green
+                    print('green')
+                    cv2.line(frame, (matching[0], matching[1]), (matching[2], matching[3]), (255, 0, 0), 2) #new line blue
+                    print("blue")
+                    midpointx1 = int((x1 + matching[0])/2)
+                    midpointy1 = int((y1 + matching[1])/2)
+                    midpointx2 = int((x2 + matching[2])/2)
+                    midpointy2 = int((y2 + matching[3])/2)
+
+
+
+
+                    print('hello')
+
+                    cv2.line(frame, (midpointx1, midpointy1), (midpointx2, midpointy2), (255, 0, 255), 1)  # mid line pink
+                    print('pink')
+
+
+                    # # Maintain a simples lookup list for points
+                    lines_list.append([(x1, y1), (x2, y2)])
+                    lines_list.append([(matching[0], matching[1]), (matching[2], matching[3])])
+
+                # if len(lines_list) >= 5:
+                #     break
+
+            else:
+                print("slopes do not match")
+
+            # # Draw the lines to connect points
+            # cv2.line(cap, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            # # Maintain a simples lookup list for points
+            # lines_list.append([(x1, y1), (x2, y2)])
 
         # Save the result image
         cv2.imshow('detectedLines', frame)
